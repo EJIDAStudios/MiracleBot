d9afb2d084b680a0579dfa9de0aa92019fada552
Unknown <ianghys@yahoo.com>
2017-06-18 10:59:33 +0800

    again update bot token

--- app.js
+++ app.js
@@ -60,4 +60,4 @@ client.on('warn', e => {
 client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-client.login(BOT_TOKEN);
+client.login(settings.token);
